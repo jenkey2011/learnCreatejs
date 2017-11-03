dc02e50c6a340a986398407fdbd7de2dd39b5379
jenkey2011 <jenkey2011@163.com>
2017-11-03 17:06:00 +0800

    add res

---
+++ src/static/js/data.js
@@ -0,0 +1,12 @@
+const list = {
+  "sprite":[
+    "sprite_simple",
+    "sprite_spriteSheetBuilder",
+    "3htmljscsses4gulp"
+  ],
+  "bbb":[
+    "4htmljscsses4gulp",
+    "5htmljscsses4gulp",
+    "6htmljscsses4gulp"
+  ]
+};
